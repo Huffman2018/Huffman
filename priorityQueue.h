f162f9b7b6d6fe0270b941d619b59c2bd505dc63
Mateus Pereira <butcraft@gmail.com>
2018-05-07 10:26:54 -0300

    Added commits to structs and functions

--- priorityQueue.h
+++ priorityQueue.h
@@ -1,13 +1,14 @@
 #include "huffmanTree.h"
 
+//Priority Queue
 typedef struct priorityQueue {
 	hTree *head;
 	int size;
 } pQueue;
 
-pQueue* createPQueue();
-hTree* createNode(char byte, int frequency);
-void enqueueNode(pQueue *queue, char byte, int frequency);
-void enqueueParentNode(pQueue *queue, hTree *node);
-hTree* dequeueNode(pQueue *queue);
-int pQueueEmpty(pQueue *queue);
+pQueue* createPQueue();										//Create Priority Queue
+hTree* createNode(char byte, int frequency);				//Create a new node
+void enqueueNode(pQueue *queue, char byte, int frequency);	//Create and add a new node in ascending order
+void enqueueParentNode(pQueue *queue, hTree *node);			//Add a node in ascending order
+hTree* dequeueNode(pQueue *queue);							//Dequeue the head of priority queue
+int pQueueEmpty(pQueue *queue);								//Verify if priority queue has empty
