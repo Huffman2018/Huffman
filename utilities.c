342a409ffcf25010d2687aa56eadf3a5d02b9bd1
Mateus Pereira <butcraft@gmail.com>
2018-05-11 02:34:16 -0300

    Rewrite functions name

--- utilities.c
+++ utilities.c
@@ -1,11 +1,9 @@
+#include <stdio.h>
 #include "utilities.h"
 
-u_char set_bit (u_char byte, int bitPos) {
-
+u_char setBit(u_char byte, int bitPos) {
     return (byte | (1 << (7 - bitPos)));
 }
-
-int is_bit_set (u_char byte, short int a) {
-
+int isBitSet(u_char byte, short int a) {
     return (byte & (1 << a));
 }
--- utilities.h
+++ utilities.h
@@ -3,5 +3,5 @@
 typedef unsigned char u_char;
 typedef unsigned int u_int;
 
-u_char set_bit (u_char byte, int bitPos);		//Set bit to bit position
-int is_bit_set (u_char byte, short int a);		//Verify if bit has set in 'a' position
+u_char setBit(u_char byte, int bitPos);		//Set bit to bit position
+int isBitSet(u_char byte, short int a);		//Verify if bit has set in 'a' position
