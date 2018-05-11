4882a4377f8de57593003a62c7a60da86cacd14f
Mateus Pereira <butcraft@gmail.com>
2018-05-11 00:26:19 -0300

    Added writeHTree function and reorganize functions sort

--- huffmanTree.c
+++ huffmanTree.c
@@ -6,12 +6,6 @@
 hTree* createHTree() {
 	return NULL;
 }
-hTree* mergeNodes(hTree *left, hTree *right) {
-	hTree *parentNode = createNode('\*', (left->frequency + right->frequency));
-	parentNode->left = left;
-	parentNode->right = right;
-	return parentNode;
-}
 hTree* constructHTree(pQueue *queue) {
 	hTree *parentNode = createHTree();
 	while(queue->head->next != NULL) {
@@ -20,11 +14,33 @@ hTree* constructHTree(pQueue *queue) {
 	}
 	return queue->head;
 }
+hTree* mergeNodes(hTree *left, hTree *right) {
+	hTree *parentNode = createNode('\*', (left->frequency + right->frequency));
+	parentNode->left = left;
+	parentNode->right = right;
+	return parentNode;
+}
 int isLeaf(hTree *tree) {
 	return ((tree->left == NULL) && (tree->right == NULL));
 }
 int hTreeEmpty(hTree *tree) {
 	return (tree == NULL);
 }
-
-
+void writeHTree(FILE *file, hTree *tree, int *treeSize) {
+	if(isLeaf(tree)) {
+		if(tree->byte == '\\' || tree->byte == '*') {
+			char byte = '\\';
+			(*treeSize)++;
+			fwrite(&byte, sizeof(char), 1, file);
+		}
+		(*treeSize)++;
+		fwrite(&tree->byte, sizeof(char), 1, file);
+		return;
+	}
+	if(tree->left != NULL) {
+		writeHTree(file, tree->left, treeSize);
+	}
+	if(tree->right != NULL) {
+		writeHTree(file, tree->right, treeSize);
+	}
+}
--- huffmanTree.h
+++ huffmanTree.h
@@ -8,8 +8,9 @@ struct huffmanTree {
 	hTree *right;
 };
 
-hTree* createHTree();							//Create Huffman Tree
-hTree* mergeNodes(hTree *left, hTree *right);	//Create a father to last 2 nodes of  priority queue
-hTree* constructHTree(pQueue *queue);			//Construct Huffman Tree using the priority queue
-int isLeaf(hTree *tree);						//Verify if a node has a leaf
-int hTreeEmpty(hTree *tree);					//Verify if huffman tree has empty
+hTree* createHTree();										//Create Huffman Tree
+hTree* constructHTree(pQueue *queue);						//Construct Huffman Tree using the priority queue
+hTree* mergeNodes(hTree *left, hTree *right);				//Create a father to last 2 nodes of  priority queue
+int isLeaf(hTree *tree);									//Verify if a node has a leaf
+int hTreeEmpty(hTree *tree);								//Verify if huffman tree has empty
+void writeHTree(FILE *file, hTree *tree, int *treeSize);	//Write Huffman Tree (pre-order) in file
