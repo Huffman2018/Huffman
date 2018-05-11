8ac33eab2d498055e27b59aa62a5f285916fffc3
Mateus Pereira <butcraft@gmail.com>
2018-05-11 01:21:51 -0300

    Add unsigned char typedef and bit set functions

--- utilities.c
+++ utilities.c
@@ -1,1 +1,11 @@
 #include "utilities.h"
+
+u_char set_bit (u_char byte, int bitPos) {
+
+    return (byte | (1 << (7 - bitPos)));
+}
+
+int is_bit_set (u_char byte, short int a) {
+
+    return (byte & (1 << a));
+}
--- utilities.h
+++ utilities.h
@@ -1,1 +1,7 @@
 #define BYTENUMBER_MAX 256
+
+typedef unsigned char u_char;
+typedef unsigned int u_int;
+
+u_char set_bit (u_char byte, int bitPos);		//Set bit to bit position
+int is_bit_set (u_char byte, short int a);		//Verify if bit has set in 'a' position
